4e0ac09baaf841e611fc7372e331a492bb20ba82
Nadhif Ginola <nfginola@gmail.com>
2022-08-12 00:51:58 -0400

    ..

--- Mira/src/main.cpp
+++ Mira/src/main.cpp
@@ -6,29 +6,6 @@ int main()
 	Application app;
 	app.run();
 
-	/*
-		
-		WIP,
-		setup window,
-		setup swapchain
-		setup renderer (frame buffer)
-
-		notes:
-			we technically break LSP when converting RenderCommandList to RenderCommandList_DX12 in execute command list,
-			but we can enforce by asserting that the command list is of the correct API type on submission
-			(future notes, if we want to support Vk and multiple simultaneous RenderDevices).
-
-			Probably better solution is to enforce that command lists submitted to a device HAS to be allocated from that specific
-			device.
-
-			hazard:
-				auto cmdl = vk_rd->allocate_cmdl(..)
-				dx_rd->submit_cmdl(cmdl)
-
-				breaks at DX impl., 
-	
-	*/
-
 
 
 
